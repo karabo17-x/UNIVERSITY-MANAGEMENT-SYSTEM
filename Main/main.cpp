64f5123f88b003e521b83c9122390aa3889a1bdc
karabocollenm@gmail.com <karabocollenm@gmail.com>
2026-05-30 05:52:30 +0200

    feat: update main application

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,11 +1,20 @@
 #include <QApplication>
 #include "MainWindow.h"
+#include <QTimer>
+#include "StudentRegistrationDialog.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     
     MainWindow window;
     window.show();
+    // If OPEN_REG_DIALOG is set, open the Register dialog immediately for debugging
+    if (qEnvironmentVariableIsSet("OPEN_REG_DIALOG")) {
+        QTimer::singleShot(0, [&window]() {
+            StudentRegistrationDialog dlg(nullptr, &window);
+            dlg.exec();
+        });
+    }
     
     return app.exec();
 }
